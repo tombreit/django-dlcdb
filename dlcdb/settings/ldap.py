b6de8d21cd929e71500df2e9a75a5222e0f80d5b
Thomas Breitner <t.breitner@csl.mpg.de>
2022-02-10 12:17:48 +0100

    use env file for ldap

--- dlcdb/settings/ldap.py
+++ dlcdb/settings/ldap.py
@@ -23,48 +23,37 @@ logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.DEBUG)
 
 
-# Concat available ldap groups
-# def get_required_groups():
-#     ldap_group_querys = []
+AUTH_LDAP_SERVER_URI = env.str("AUTH_LDAP_SERVER_URI")
+AUTH_LDAP_BIND_DN = env.str('AUTH_LDAP_BIND_DN')
+AUTH_LDAP_BIND_PASSWORD = env.str('AUTH_LDAP_BIND_PASSWORD')
 
-#     for group in env('LDAP_ACCESS_GROUPS'):
-#         ldap_group_querys.append(
-#             LDAPGroupQuery(
-#                 'cn={0},cn=Users,dc=mpicc,dc=de'.format(group)
-#             )
-#         )
-#     print("ldap_group_querys: ", ldap_group_querys)
-
-
-AUTHENTICATION_BACKENDS += [
-    'django_auth_ldap.backend.LDAPBackend',
-]
+# First check LDAPBackend, than ModelBackend
+AUTHENTICATION_BACKENDS = ['django_auth_ldap.backend.LDAPBackend'] + AUTHENTICATION_BACKENDS
+AUTH_LDAP_GROUP_TYPE = ActiveDirectoryGroupType()
+AUTH_LDAP_FIND_GROUP_PERMS = True
 
 
 AUTH_LDAP_GLOBAL_OPTIONS = {
     # ldap.OPT_PROTOCOL_VERSION: 3,
     # ldap.OPT_REFERRALS: 0,
-    # ldap.OPT_X_TLS_CACERTFILE: '/etc/ssl/localcerts/radiusi.pem',
+    # ldap.OPT_X_TLS_CACERTFILE: '/path/to/pemfile',
 }
 
-if env('SETTINGS_MODE') == 'dev':
+if env.str('SETTINGS_MODE') == 'dev':
     ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
-    # ldap.set_option(ldap.OPT_DEBUG_LEVEL, 255)
+    ldap.set_option(ldap.OPT_DEBUG_LEVEL, 255)
 
-
-AUTH_LDAP_SERVER_URI = "ldaps://rodc.mpicc.de"
-
-AUTH_LDAP_BIND_DN = 'cn={},ou=Hidden AD Objects,dc=mpicc,dc=de'.format(
-    env('LDAP_QUERY_USERNAME')
-)
-AUTH_LDAP_BIND_PASSWORD = env('LDAP_QUERY_PASSWORD')
 
 AUTH_LDAP_USER_SEARCH = LDAPSearchUnion(
-    LDAPSearch("cn=Users,dc=mpicc,dc=de", ldap.SCOPE_SUBTREE, "(sAMAccountName=%(user)s)"),
+    LDAPSearch(
+        f"{env.str('AUTH_LDAP_USERS_DN')}",
+        ldap.SCOPE_SUBTREE,
+        "(sAMAccountName=%(user)s)",
+    ),
 )
 
 AUTH_LDAP_GROUP_SEARCH = LDAPSearch(
-    "cn=Users,dc=mpicc,dc=de",
+    f"{env.str('AUTH_LDAP_USERS_DN')}",
     ldap.SCOPE_SUBTREE,
     "(objectClass=group)"
 )
@@ -82,21 +71,19 @@ AUTH_LDAP_USER_ATTR_MAP = {
 
 # https://django-auth-ldap.readthedocs.io/en/latest/groups.html#limiting-access
 AUTH_LDAP_REQUIRE_GROUP = (
-    LDAPGroupQuery("cn=itadmin,cn=Users,dc=mpicc,dc=de") | 
-    LDAPGroupQuery("cn=edv,cn=Users,dc=mpicc,dc=de") |
-    LDAPGroupQuery("cn=dlcdb,cn=Users,dc=mpicc,dc=de") 
+    LDAPGroupQuery(env.str("AUTH_LDAP_GROUP_SUPERUSERS")) | 
+    LDAPGroupQuery(env.str("AUTH_LDAP_GROUP_STAFF"))
 )
 
 AUTH_LDAP_USER_FLAGS_BY_GROUP = {
     "is_active": (
-        LDAPGroupQuery("cn=edv,cn=Users,dc=mpicc,dc=de") |
-        LDAPGroupQuery("cn=dlcdb,cn=Users,dc=mpicc,dc=de") 
+        LDAPGroupQuery(env.str("AUTH_LDAP_GROUP_SUPERUSERS")) | 
+        LDAPGroupQuery(env.str("AUTH_LDAP_GROUP_STAFF"))
     ),
     "is_staff": (
-        LDAPGroupQuery("cn=edv,cn=Users,dc=mpicc,dc=de") |
-        LDAPGroupQuery("cn=dlcdb,cn=Users,dc=mpicc,dc=de") 
+        LDAPGroupQuery(env.str("AUTH_LDAP_GROUP_STAFF"))
     ),
     "is_superuser": (
-        LDAPGroupQuery("cn=django_admins,ou=Hidden AD Objects,dc=mpicc,dc=de")
+        LDAPGroupQuery(env.str("AUTH_LDAP_GROUP_SUPERUSERS"))
     ),
 }
