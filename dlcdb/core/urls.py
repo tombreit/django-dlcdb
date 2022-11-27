f69ff768630a5b2235b8b79c1c4b726f5f33a829
Thomas Breitner <t.breitner@csl.mpg.de>
2022-11-28 00:04:01 +0100

    some branding design fixes

--- dlcdb/core/urls.py
+++ dlcdb/core/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import (
     room_views,
     relocate_views,
+    procure_views,
+    lent_management_views,
 )
 
 
@@ -10,4 +12,6 @@ app_name = 'core'
 urlpatterns = [
     path('rooms/reconcile/<int:reconcile_id>', room_views.ReconcileRoomsView.as_view(), name='reconcile-rooms'),
     path('devices/relocate/', relocate_views.DevicesRelocateView.as_view(), name='core_devices_relocate'),
+    path('orderedrecord/procure/', procure_views.ProcureDeviceView.as_view(), name='core_procure_device'),
+    path('lentrecord/print/<int:pk>/', lent_management_views.PrintLentSheetView.as_view(), name='core_print_lent_sheet'),
 ]
--- dlcdb/urls.py
+++ dlcdb/urls.py
@@ -5,18 +5,14 @@ from django.contrib import admin
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
 
-from dlcdb.core.views import (
-    dashboard_views,
-    procure_views,
-    lent_management_views,
-    relocate_views,
-)
+from dlcdb.core.views import dashboard_views
 
 
 admin.site.site_header = "DLCDB Admin"
 admin.site.site_title = "DLCDB Admin"
 admin.site.index_title = "DLCDB Administration"
 admin.site.enable_nav_sidebar = False
+admin.site.logout_template = 'accounts/logout.html'
 
 
 urlpatterns = [
@@ -24,10 +20,6 @@ urlpatterns = [
 
     # Commented out for having a functional listing of installed admin apps:
     # path('admin/', dashboard_views.DashboardView.as_view(), name='core_dashboard'),
-
-    path('admin/core/orderedrecord/procure/', procure_views.ProcureDeviceView.as_view(), name='core_procure_device'),
-    path('admin/core/lentrecord/print/<int:pk>/', lent_management_views.PrintLentSheetView.as_view(), name='core_print_lent_sheet'),
-
     path('core/', include('dlcdb.core.urls')),
     path('inventory/', include('dlcdb.inventory.urls')),
     path('lending/', include('dlcdb.lending.urls')),
