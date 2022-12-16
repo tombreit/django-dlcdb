1768e16f94523108d5c0f858646f3b23639055c0
Thomas Breitner <t.breitner@csl.mpg.de>
2022-12-16 09:13:36 +0100

    ORM optimization for records display: select related user instances

--- dlcdb/core/admin/record_admin.py
+++ dlcdb/core/admin/record_admin.py
@@ -99,7 +99,7 @@ class RecordAdmin(NoModificationModelAdminMixin, CustomRecordModelAdmin):
     ordering = ('-created_at',)
 
     def get_queryset(self, request):
-        return super().get_queryset(request).select_related('device', 'room')
+        return super().get_queryset(request).select_related('device', 'room', 'user')
 
     def has_add_permission(self, request):
         return False
