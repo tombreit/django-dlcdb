fdfbb467fc6d48072296b5c412992ff1444489a2
Thomas Breitner <t.breitner@csl.mpg.de>
2022-12-30 17:36:32 +0100

    do not expose 'save as new' and 'save and add another' for inroom-, lost-, and removed-records

--- dlcdb/core/admin/inroomrecord_admin.py
+++ dlcdb/core/admin/inroomrecord_admin.py
@@ -16,5 +16,14 @@ class InRoomRecordAdmin(RedirectToDeviceMixin, CustomBaseProxyModelAdmin):
         'room',
     ]
 
+    def render_change_form(
+        self, request, context, add=False, change=False, form_url="", obj=None
+    ):
+        context.update({
+            'show_save_and_add_another': False,
+            'show_save_and_continue': False,
+        })
+        return super().render_change_form(request, context, add, change, form_url, obj)
+
     def has_add_permission(self, request):
         return True
--- dlcdb/core/admin/lostrecord_admin.py
+++ dlcdb/core/admin/lostrecord_admin.py
@@ -20,3 +20,12 @@ class LostRecordAdmin(RedirectToDeviceMixin, NoModificationModelAdminMixin, Cust
         'device',
         'note',
     ]
+
+    def render_change_form(
+        self, request, context, add=False, change=False, form_url="", obj=None
+    ):
+        context.update({
+            'show_save_and_add_another': False,
+            'show_save_and_continue': False,
+        })
+        return super().render_change_form(request, context, add, change, form_url, obj)
