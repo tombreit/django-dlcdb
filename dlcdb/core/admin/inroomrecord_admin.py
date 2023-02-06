486a4072e69a2588a7508de1e604e068b3d3c8ae
Thomas Breitner <t.breitner@csl.mpg.de>
2023-02-06 10:56:27 +0100

    Added meaningful success message for new INROOM records.

--- dlcdb/core/admin/inroomrecord_admin.py
+++ dlcdb/core/admin/inroomrecord_admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.contrib import messages
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
-from ..models import InRoomRecord, Device
+from ..models import InRoomRecord, Room
 from ..forms.proxyrecord_admin_form import ProxyRecordAdminForm
 from .base_admin import CustomBaseProxyModelAdmin, RedirectToDeviceMixin
 
@@ -9,12 +12,33 @@ from .base_admin import CustomBaseProxyModelAdmin, RedirectToDeviceMixin
 class InRoomRecordAdmin(RedirectToDeviceMixin, CustomBaseProxyModelAdmin):
     form = ProxyRecordAdminForm
     change_form_template = 'core/inroomrecord/change_form.html'
-    list_display = ['device', 'created_at', 'note']
-    fields = ('device', 'room', 'note')
+    list_display = [
+        'device',
+        'created_at',
+        'note',
+    ]
+    fields = [
+        'device',
+        'room',
+        'note',
+    ]
 
     autocomplete_fields = [
         'room',
     ]
+
+    def has_add_permission(self, request):
+        return True
+
+    def response_add(self, request, obj, post_url_continue=None):
+        if '_save' in request.POST:
+            redirect_url = reverse('admin:core_device_changelist')
+            device = obj.device
+            room = Room.objects.get(pk=request.POST.get("room"))
+            messages.success(request, f'Raumänderung nach Raum “{room}” für Device “{device}” durchgeführt.')
+            return HttpResponseRedirect(redirect_url)
+        else:
+            return super().response_change(request, obj, post_url_continue=post_url_continue)
 
     def render_change_form(
         self, request, context, add=False, change=False, form_url="", obj=None
@@ -25,5 +49,3 @@ class InRoomRecordAdmin(RedirectToDeviceMixin, CustomBaseProxyModelAdmin):
         })
         return super().render_change_form(request, context, add, change, form_url, obj)
 
-    def has_add_permission(self, request):
-        return True
