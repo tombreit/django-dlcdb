452e63649b88c108491837f40f57e28762b45acc
Thomas Breitner <t.breitner@csl.mpg.de>
2023-05-11 18:57:04 +0200

    (WIP) cleanup, some qr js refactoring

--- dlcdb/inventory/urls.py
+++ dlcdb/inventory/urls.py
@@ -9,10 +9,7 @@ urlpatterns = [
     path('room/<uuid:uuid>', views.InventorizeRoomView.as_view(), name='inventorize-room'),
     path('room/<int:pk>/qrs/', views.QrCodesForRoomDetailView.as_view(), name='qr-room-printout'),
     path('room/<int:pk>', views.InventorizeRoomView.as_view(), name='inventorize-room'),
-    
     path('note/<str:obj_type>/<uuid:obj_uuid>/', views.update_note_view, name='note-update'),
-    # path('note/device-<uuid:device_uuid>/', views.update_note_view, name='note-update'),
-    
     path('misc/inventory_lending_report', views.InventoryReportView.as_view(), name="inventory-lending-report"),
     path('devices', views.DevicesSearchView.as_view(), name='search-devices'),
     path('saplist/compare_sap_list/<int:pk>/', views.SapCompareListView.as_view(), name='compare-sap-list'),
