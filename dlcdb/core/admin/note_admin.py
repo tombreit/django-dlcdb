daa3e6a3e26283549d74718f8319fbc372cf02fd
Thomas Breitner <t.breitner@csl.mpg.de>
2023-07-12 12:33:48 +0200

    display device-room in notes admin

--- dlcdb/core/admin/note_admin.py
+++ dlcdb/core/admin/note_admin.py
@@ -5,7 +5,37 @@ from ..models import Note
 
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
-    list_display = ["pk", "text", "inventory", "device", "room", "created_at", "updated_at"]
-    list_filter = ["inventory", "room"]
-    search_fields = ["text", "device__edv_id", "device__sap_id", "room__number"]
-    readonly_fields = ["created_by", "updated_by", "created_at", "updated_at"]
+    list_display = [
+        "text",
+        "device",
+        "get_device_room",
+        "inventory",
+        "room",
+        "updated_at",
+    ]
+    list_filter = [
+        "inventory",
+        # TODO: Filter got same filter title as the next filter: room
+        # "device__active_record__room",
+        "room",
+        "updated_at",
+    ]
+    search_fields = [
+        "text",
+        "device__edv_id",
+        "device__sap_id",
+        "room__number",
+        "room__nickname",
+    ]
+    readonly_fields = [
+        "created_by",
+        "updated_by",
+        "created_at",
+        "updated_at",
+    ]
+
+    @admin.display(description="Device room")
+    @admin.display(ordering="device__active_record__room")
+    def get_device_room(self, obj):
+        if hasattr(obj, "device") and obj.device:
+                return obj.device.active_record.room
