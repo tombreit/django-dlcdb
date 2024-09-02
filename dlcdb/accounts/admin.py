ef2c149ca72d76baf144e15353d2eca7b50be6f0
Thomas Breitner <t.breitner@csl.mpg.de>
2024-09-02 11:53:15 +0200

    Moved django group admin in our accounts admin section

--- dlcdb/accounts/admin.py
+++ dlcdb/accounts/admin.py
@@ -4,6 +4,10 @@
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group as DjangoGroup
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.utils.translation import gettext_lazy as _
+
 from .models import CustomUser
 
 
@@ -16,3 +20,26 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
+
+
+# Move the Django upstream "Group" admin in our "Account" admin section
+
+
+class Group(DjangoGroup):
+    """Instead of trying to get new user under existing `Aunthentication and Authorization`
+    banner, create a proxy group model under our Accounts app label.
+    Refer to: https://github.com/tmm/django-username-email/blob/master/cuser/admin.py
+    """
+
+    class Meta:
+        verbose_name = _("group")
+        verbose_name_plural = _("groups")
+        proxy = True
+
+
+admin.site.unregister(DjangoGroup)
+
+
+@admin.register(Group)
+class GroupAdmin(BaseGroupAdmin):
+    pass
