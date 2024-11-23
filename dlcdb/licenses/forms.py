ff802675ebcafe8a2d24155aab5883d43ad1510e
Thomas Breitner <t.breitner@csl.mpg.de>
2024-11-23 12:14:45 +0100

    Some license form customizations, limit device_type choices to license relevant choices

--- dlcdb/licenses/forms.py
+++ dlcdb/licenses/forms.py
@@ -1,25 +1,41 @@
 from django import forms
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout
+from crispy_forms.layout import Layout, Row, Column
 from crispy_bootstrap5.bootstrap5 import FloatingField
 
-from dlcdb.core.models import Device
+from django.db.models import Q
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
+
+from dlcdb.core.models import Device, DeviceType
 
 
 class LicenseForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        # Limit device-type choices to "License-type" choices
+        self.fields["device_type"].queryset = DeviceType.objects.filter(
+            Q(name__startswith="Lizenz::") | Q(name__startswith="License::") | Q(name__startswith="Licence::")
+        )
+
+        # Crispy forms
         self.helper = FormHelper()
         self.helper.form_tag = False
         self.helper.layout = Layout(
+            Row(
+                Column(FloatingField("sap_id"), css_class="col-md-6"),
+            ),
             FloatingField("subscribers"),
-            FloatingField("sap_id"),
-            FloatingField("manufacturer"),
-            FloatingField("series"),
-            FloatingField("maintenance_contract_expiration_date"),
-            FloatingField("note"),
+            Row(
+                Column(FloatingField("maintenance_contract_expiration_date"), css_class="col-md-6"),
+                Column(FloatingField("device_type"), css_class="col-md-6"),
+            ),
+            Row(
+                Column(FloatingField("manufacturer"), css_class="col-md-6"),
+                Column(FloatingField("series"), css_class="col-md-6"),
+            ),
+            Row(Column("note", css_class="col-md-6")),
         )
 
     subscribers = forms.CharField(
@@ -44,5 +60,16 @@ class LicenseForm(forms.ModelForm):
 
     class Meta:
         model = Device
-        fields = ["sap_id", "manufacturer", "series", "maintenance_contract_expiration_date", "note", "subscribers"]
-        widgets = {"maintenance_contract_expiration_date": forms.DateInput(attrs={"type": "date"})}
+        fields = [
+            "sap_id",
+            "manufacturer",
+            "series",
+            "maintenance_contract_expiration_date",
+            "note",
+            "device_type",
+            "subscribers",
+        ]
+        widgets = {
+            "maintenance_contract_expiration_date": forms.DateInput(attrs={"type": "date"}),
+            "note": forms.Textarea(attrs={"rows": 6}),
+        }
