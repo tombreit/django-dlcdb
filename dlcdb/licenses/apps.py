d43912ca1a5459a0352d9c9d4442ee4cb9d0fd55
Thomas Breitner <t.breitner@csl.mpg.de>
2024-11-24 17:18:01 +0100

    Some translations

--- dlcdb/licenses/apps.py
+++ dlcdb/licenses/apps.py
@@ -3,11 +3,12 @@
 # SPDX-License-Identifier: EUPL-1.2
 
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class LicensesConfig(AppConfig):
     name = "dlcdb.licenses"
-    verbose_name = "License Management"
+    verbose_name = _("License Management")
 
     nav_entries = [
         {
--- dlcdb/smallstuff/apps.py
+++ dlcdb/smallstuff/apps.py
@@ -3,11 +3,13 @@
 # SPDX-License-Identifier: EUPL-1.2
 
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class SmallstuffConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "dlcdb.smallstuff"
+    verbose_name = _("Smallstuff")
 
     nav_entries = [
         {
