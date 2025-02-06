2068e855006dfa5f228f565ec2a023959e8dc60f
Thomas Breitner <t.breitner@csl.mpg.de>
2025-02-06 11:43:38 +0100

    Fixed None error if obj does not resolve

--- dlcdb/reporting/utils/representations.py
+++ dlcdb/reporting/utils/representations.py
@@ -29,17 +29,17 @@ def get_record_data_row(record, event):
             for model_name in item.get("model"):
                 obj = getattr(obj, model_name)
 
-            field_name = item.get("field")
-            print(f"{field_name=}")
-            field = getattr(obj, item.get("field"))
+            if obj:
+                field_name = item.get("field")
+                field = getattr(obj, item.get("field"))
 
-            if field_name == "contract_expiration_date":
-                field = f"Expiry date: {field:%Y-%m-%d}"
-            elif isinstance(field, datetime.date):
-                field = f"{field:%Y-%m-%d}"
+                if field:
+                    if field_name == "contract_expiration_date":
+                        field = f"Expiry date: {field:%Y-%m-%d}"
+                    elif isinstance(field, datetime.date):
+                        field = f"{field:%Y-%m-%d}"
 
-            print(f"field: {field}")
-            row.append(field)
+                    row.append(field)
 
     return row
 
