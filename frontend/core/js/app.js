bc5bea12c66ace5758d4aaa447b26d153942460d
Thomas Breitner <t.breitner@csl.mpg.de>
2025-02-08 17:39:57 +0100

    Removed unused methods from core/app.js

--- frontend/core/js/app.js
+++ frontend/core/js/app.js
@@ -1,8 +1,1 @@
 import * as bootstrap from 'bootstrap'
- 
-import htmx from "htmx.org";
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("[ i ] document ready");
-});
